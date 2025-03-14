bed88ed442ae370a6096ee4ba6e5a613df8bd9b7
dawei <a6445144b@163.com>
2025-03-15 00:11:07 +0800

    feat: 网站添加百度监控

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import '@/styles/icon/iconfont.css';
 import 'bytemd/dist/index.css';
 import '@/styles/bytemd.css';
 import { GlobalContextProvider } from "@/context/globalContext";
+import Script from 'next/script'
 // import 'highlight.js/styles/default.css'; // 亮色主题
 import 'highlight.js/styles/vs2015.css'; // 更推荐暗色主题
-
 
 export default async function RootLayout({
   children,
@@ -14,6 +14,23 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="baidu-tongji"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _hmt = _hmt || [];
+              (function() {
+                var hm = document.createElement("script");
+                hm.src = "https://hm.baidu.com/hm.js?${process.env.BAIDU_TONGJI_ID}";
+                var s = document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(hm, s);
+              })();
+            `,
+          }}
+        />
+      </head>
       <body className="text-text-normal bg-bg-normal">
         <GlobalContextProvider>
           {children}
